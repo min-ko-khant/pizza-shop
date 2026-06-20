5abd33d366215a6df8225949e54c179c3668cd85
Min Ko Khant <minkokhant1007@gmail.com>
2026-06-21 01:17:39 +0630

    added contact form

--- js/main.js
+++ js/main.js
@@ -23,3 +23,30 @@ function prevCarousel(){
 }
 
 setInterval(() => nextCarousel(),3000)
+
+const form = document.querySelector("#form")
+form.addEventListener("submit",function (e){
+    e.preventDefault();
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries())
+    let isValid = true;
+    const validations = {
+        name:data.name.trim() !== "",
+        email:data.email.trim() !== "",
+        subject:data.subject.trim() !== "",
+        message:data.message.trim() !== ""
+    };
+    for(field in validations){
+        if(!validations[field]){
+            document.getElementById(field).style.borderColor = "red";
+            isValid = false;
+        } else {
+            document.getElementById(field).style.borderColor = "#202020";
+        }
+    }
+    if (isValid){
+        alert("SUCCESS! Message successfully sent");
+        this.reset();
+    }
+})
+
